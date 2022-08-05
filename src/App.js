fe4b7bcc76196e691a98438ec63ab4e4fccd82de
Markus Masalkovski <marmas99@gmail.com>
2022-08-05 22:30:16 +0200

    code to cloud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import './comonents/main_header_bar/MainHeaderBar.css'
+import './comonents/main_header_bar/MainHeaderBar'
+import MainHeaderBar from './comonents/main_header_bar/MainHeaderBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id='main_title'>Markus Masalkovski</h1>
+      <hr className='header_line'></hr>
+        <MainHeaderBar id='main_header_bar'/>
+      <hr className='header_line'></hr>
     </div>
   );
 }
---
+++ src/comonents/main_header_bar/MainHeaderBar.js
@@ -0,0 +1,11 @@
+function MainHeaderBar() {
+    return (
+        <div>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+        </div>
+    );
+}
+
+export default MainHeaderBar
