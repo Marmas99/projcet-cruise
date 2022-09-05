cc967c2f9966d30e7664d3fda37c05ed3c029567
Markus Masalkovski <marmas99@gmail.com>
2022-09-05 21:21:38 +0200

    added underline for button

--- src/comonents/main_header_bar/MainHeaderBar.js
+++ src/comonents/main_header_bar/MainHeaderBar.js
@@ -1,7 +1,7 @@
 function MainHeaderBar() {
     return (
         <div id="main_header_bar">
-            <button className="ping">About Me</button>
+            <button>About Me</button>
             <button>Resume</button>
             <button>Personal projects</button>
         </div>
