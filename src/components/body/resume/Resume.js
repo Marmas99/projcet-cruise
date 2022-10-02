cfe3d25c90e0cb0a23f2c294b54da8e68a2278b3
Markus Masalkovski <marmas99@gmail.com>
2022-10-02 18:08:45 +0200

    added grid layout

--- src/components/body/resume/Resume.js
+++ src/components/body/resume/Resume.js
@@ -1,6 +1,9 @@
 function Resume(){
     return (
         <div className="container">
+            <div id="item1" className="gridItem">Item 1</div> 
+            <div id="item2" className="gridItem">Item 2</div>
+            <div id="item3" className="gridItem">Item 3</div>
         </div>
     )
 }
